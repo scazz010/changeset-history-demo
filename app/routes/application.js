a628a810773e3b67724287d8a99ae216384ecb4f
Sam Carr <samcarr4@googlemail.com>
2017-06-26 10:45:41 -0700

    Added example of working with validation errors

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 import ChangesetHistory from 'ember-changeset-history';
+  import { validatePresence, validateLength } from 'ember-changeset-validations/validators';
+import lookupValidator from 'ember-changeset-validations';
 
 export default Ember.Route.extend({
 
@@ -8,7 +10,9 @@ export default Ember.Route.extend({
   },
 
   setupController(controller, model) {
-    controller.set('changeset', new ChangesetHistory(model));
+    controller.set('changeset', new ChangesetHistory(model, lookupValidator({
+      firstName: [validatePresence(true), validateLength({min: 2})]
+    }), {}, {maxHistoryLength: 3}));
   }
 
 });
